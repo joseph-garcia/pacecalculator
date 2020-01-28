60f72660fd33ad8de6283dc9fd7ab425c9bc9230
Joseph Garcia <robhaywood12@gmail.com>
2020-01-28 14:50:41 -0600

    hamburger icon in
    overwrites pre-existing visual

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.view.Gravity
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.widget.ToolbarWidgetWrapper
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import kotlinx.android.synthetic.main.activity_calculate.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // create a member variable for the draw layout itself
+    private var drawer: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,19 @@ class MainActivity : AppCompatActivity() {
 
         var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        var toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        // take draw variable, and pass toggle variable in
+        drawer_layout.addDrawerListener(toggle)
+        // takes care of rotating hamburger icon
+        toggle.syncState()
 
 
         // Create Hour Picker
@@ -108,6 +127,16 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    // when we press the back btn when drawer is open, we dont want to leave the activity--instead we want to close nav drawer
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START))
+        {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
 
