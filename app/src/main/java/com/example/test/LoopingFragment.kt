7d11ae0937ad7481b7bfd1beb58c948d571d9872
Joseph Garcia <robhaywood12@gmail.com>
2020-02-03 11:45:57 -0600

    listview added
    -not yet tested

--- app/src/main/java/com/example/test/LoopingFragment.kt
+++ app/src/main/java/com/example/test/LoopingFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_looping.*
 
 class LoopingFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,25 @@ class LoopingFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_looping, container, false)
+
+        var view = inflater.inflate(R.layout.fragment_looping, container, false)
+
+        var menuItems = arrayOf<String>(
+            "Do something",
+            "Do something else",
+            "Do yet another thing"
+            )
+
+        var listView : ListView = entryMenu
+
+        var listViewAdapter = ArrayAdapter<String>(
+            requireActivity(),
+            android.R.layout.simple_list_item_1,
+            menuItems
+        )
+
+        listView.setAdapter(listViewAdapter)
+
+        return view
     }
 }
